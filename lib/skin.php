4e13b0e8494804ea294977f618375115fe823bd1
James West <james@jameswest.co.nz>
2014-10-21 16:29:40 +1300

    Updated get_asset_uri() to allow for symlinked themes

--- lib/skin.php
+++ lib/skin.php
@@ -4,43 +4,73 @@ class BranchSkin {
 	public $name = 'default';
 	public $link = '/wp-content/themes/branch/skins/default';
 	
+	public $paths = array();
+	
 	public function __construct($name='default') {
+	
 		$this->name = $name;
 		
 		// could be more dynamic - this assume that the skin resides in the current themes directory, which is not true for branch.
 		$theme = wp_get_theme();
-		$this->link = site_url('/wp-content/themes/' . $theme->stylesheet . '/skins/'.$name);
+		$this->link = site_url('wp-content/themes/' . $theme->stylesheet . '/skins/'.$name);
 		
+        // set assets paths - must be relative to current theme directory
+        $this->add_asset_path("../../uploads/branch/skins/{$this->name}");
+        $this->add_asset_path("../{$theme->stylesheet}/skins/{$this->name}");
+        $this->add_asset_path("../branch/skins/{$this->name}");
+        $this->add_asset_path("../{$theme->stylesheet}/skins/default");
+        $this->add_asset_path("../branch/skins/default");
+        
         // set timeber views
         Timber::$dirname = array();
-        
-        // uploaded overiddes
-		Timber::$dirname[] = '../../uploads/branch/skins/' . $this->name;
-		
-		// skin specific
-		Timber::$dirname[] = '../' . $theme->stylesheet . '/skins/' . $this->name;
-		
-		// parent theme skin default
-		if(is_child_theme()) Timber::$dirname[] = '../branch/skins/' . $this->name;// skin specific
-		
-		// default to default
-		Timber::$dirname[] = '../' . $theme->stylesheet . '/skins/default';
-		
-		// parent theme default
-		if(is_child_theme()) Timber::$dirname[] = '../branch/skins/default';// skin specific
+        foreach($this->paths as $path) {
+	        Timber::$dirname[] = $path;
+        }
 		
 		return $this;
 	}
 	
+	private function add_asset_path($path) {
+		if(!in_array($path, $this->paths)) {
+			$this->paths[] = $path;
+		}
+	}
+	
+	/*
+	 * get_asset_uri
+	 * 
+	 * Determines the final asset URI based on the override order defined in __contruct()
+	 * Returns first match.
+	 */
 	public static function get_asset_uri($path) {
+		$theme = wp_get_theme();
 		foreach(Timber::$dirname as $dir) {
-			chdir(realpath(__DIR__ . '/..'));
-			$dir = realpath($dir);
+			$levels = 0;
 			
-			$file = $dir . $path;
+			foreach(explode('../', $dir) as $parent) {
+				if($parent == '') $levels++;
+			}
+			
+			$theme_dir = WP_CONTENT_DIR . "/themes/{$theme->stylesheet}";
+			
+			// remove levels
+			$theme_dir = explode('/', $theme_dir);
+			while($levels > 0) {
+				array_pop($theme_dir);
+				$levels--;
+			}
+			
+			$theme_dir = implode('/', $theme_dir) . '/';
+			$dir = str_replace('../', '', $dir);
+			
+			// final path & URI
+			$uri = '/' . str_replace(ABSPATH, '', $theme_dir . $dir) . $path;
+			$dir = realpath($theme_dir . $dir);
+			
+			$file = realpath($dir . $path);
 			
 			if(file_exists($file)) {
-				return '/' . str_replace(ABSPATH, '', $file);
+				return $uri;
 			}
 		}
 	}
