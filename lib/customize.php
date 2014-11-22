0cb7b0033c90abdefc45db2f2e60aa3e60d2d254
James West <james@jameswest.co.nz>
2014-11-22 21:25:59 +1300

    General tidy, integrated with CSS class

--- lib/customize.php
+++ lib/customize.php
@@ -6,146 +6,167 @@
  * @since branch 1.0
  */
 class BranchCustomize {
-	static function branch($site) {
-		$site->customize = new self();
-	}
-	
-	public function __construct() {
+	public function __construct($skin) {
+		// make skin available to future calls
+		$this->skin = $skin;
+		
 		// Setup the Theme Customizer settings and controls...
 		add_action( 'customize_register' , array( $this , 'register' ) );
-		
-		// Output custom CSS to live site
-		add_action( 'wp_head' , array( $this , 'header_output' ) );
 		
 		// Enqueue live preview javascript in Theme Customizer admin screen
 		add_action( 'customize_preview_init' , array( $this , 'live_preview' ) );
 	}
 
-   /**
-	* This hooks into 'customize_register' (available as of WP 3.4) and allows
-	* you to add new sections and controls to the Theme Customize screen.
-	* 
-	* Note: To enable instant preview, we have to actually write a bit of custom
-	* javascript. See live_preview() for more.
-	*  
-	* @see add_action('customize_register',$func)
-	* @param \WP_Customize_Manager $wp_customize
-	* @link http://ottopress.com/2012/how-to-leverage-the-theme-customizer-in-your-own-themes/
-	* @since branch 1.0
-	*/
-   public static function register( $wp_customize ) {
-	  //1. Define a new section (if desired) to the Theme Customizer
-	  $wp_customize->add_section( 'branch_skins', 
-		 array(
-			'title' => __( 'Skin Selection', 'branch' ), //Visible title of section
-			'priority' => 0, //Determines what order this appears in
-			'capability' => 'edit_theme_options', //Capability needed to tweak
-			//'description' => __('Allows you to customize some example settings for branch.', 'branch'), //Descriptive tooltip
-		 ) 
-	  );
-	  
-	  //2. Register new settings to the WP database...
-	  $wp_customize->add_setting( 'skin', //No need to use a SERIALIZED name, as `theme_mod` settings already live under one db record
-		 array(
-			'default' => 'default', //Default setting/value to save
-			'type' => 'theme_mod', //Is this an 'option' or a 'theme_mod'?
-			'capability' => 'edit_theme_options', //Optional. Special permissions for accessing this setting.
-			'transport' => 'postMessage', //What triggers a refresh of the setting? 'refresh' or 'postMessage' (instant)?
-		 ) 
-	  );	  
-			
-	  //3. Finally, we define the control itself (which links a setting to a section and renders the HTML controls)...
-	  $options = array();
-	  foreach(BranchSkin::get_skins() as $skin) {
-		  $options[$skin['name']] = $skin['name'];
-	  }
-	  $wp_customize->add_control(
+	/**
+	 * This hooks into 'customize_register' (available as of WP 3.4) and allows
+	 * you to add new sections and controls to the Theme Customize screen.
+	 * 
+	 * Note: To enable instant preview, we have to actually write a bit of custom
+	 * javascript. See live_preview() for more.
+	 *  
+	 * @see add_action('customize_register',$func)
+	 * @param \WP_Customize_Manager $wp_customize
+	 * @link http://ottopress.com/2012/how-to-leverage-the-theme-customizer-in-your-own-themes/
+	 * @since branch 1.0
+	 */
+	public function register( $wp_customize ) {
+		// let's make the blogname auto update - requires JS and relevant classes in the skin
+		$wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
+		
+		// now get skin fields
+		$wp_customize->add_panel( 'branch', 
+			array(
+				'title' => __( 'Skin Options', 'branch' ),
+				'priority' => 2,
+				'capability' => 'edit_theme_options',
+			) 
+		);
+		
+		// skin selector
+		$wp_customize->add_section( 'branch_skins', 
+			array(
+				'title'			=> __( 'Select Skin', 'branch' ),
+				'priority'		=> 0,
+				'capability'	=> 'edit_theme_options',
+				'description' => __('Changing skins may require a "Save & Publish" to enable all features.', 'branch'), //Descriptive tooltip
+				'panel' => 'branch'
+			) 
+		);
+		
+		$wp_customize->add_setting( 'skin',
+			array(
+				'default'		=> 'default',
+				'type'			=> 'theme_mod',
+				'capability'	=> 'edit_theme_options',
+				'transport'		=> 'refresh',//postMessage
+			) 
+		);	  
+		
+		$options = array();
+		foreach($this->skin->skins() as $skin) {
+			$options[$skin['name']] = $skin['name'];
+		}
+		$wp_customize->add_control(
 		    'skin',
 		    array(
 		        'type' => 'select',
-		        //'label' => 'Theme skin:',
 		        'section' => 'branch_skins',
 		        'choices' => $options,
 		    )
 		);
-	  
-	  //4. We can also change built-in settings by modifying properties. For instance, let's make some stuff use live preview JS...
-	  $wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
-	  $wp_customize->get_setting( 'skin' )->transport = 'postMessage';
-   }
-
-   /**
-	* This will output the custom WordPress settings to the live theme's WP head.
-	* 
-	* Used by hook: 'wp_head'
-	* 
-	* @see add_action('wp_head',$func)
-	* @since branch 1.0
-	*/
-   public static function header_output() {
-   	  return false;// don't want this yet
-	  ?>
-	  <!--Customizer CSS--> 
-	  <style type="text/css">
-		   <?php self::generate_css('#site-title a', 'color', 'header_textcolor', '#'); ?> 
-		   <?php self::generate_css('body', 'background-color', 'background_color', '#'); ?> 
-		   <?php self::generate_css('a', 'color', 'link_textcolor'); ?>
-	  </style> 
-	  <!--/Customizer CSS-->
-	  <?php
-   }
+		
+		// need to load in all skin controls so that all are accessible if the user changes skins
+		// would need to prefix all sections with the skin name to allow for showing/hiding on the fly
+		
+		$skin_customize = $this->skin->config()['customize'];
+		
+		if(isset($skin_customize['sections']) && !empty($skin_customize['sections'])) {
+			foreach($skin_customize['sections'] as $key => $section) {
+				$wp_customize->add_section( $section['id'], 
+					array(
+						'title' => __( $section['title'], 'branch' ),
+						'priority' => $key+1,
+						'capability' => 'edit_theme_options',
+						'description' => __($section['description'], 'branch'), //Descriptive tooltip
+						'panel' => 'branch'
+					) 
+				);
+				
+				if(isset($section['fields']) && !empty($section['fields'])) {
+					foreach($section['fields'] as $field) {
+						$wp_customize->add_setting( $field['id'],
+							array(
+								'default' => @$field['default'],
+								'type' => 'theme_mod',
+								'capability' => 'edit_theme_options',
+								'transport' => 'refresh',
+							) 
+						);
+						
+						switch($field['type']) {
+							case 'text':
+								$wp_customize->add_control(
+									$field['id'],
+									array(
+										'label'      => __( $field['label'], 'branch' ),
+										'section'    => $section['id'],
+									)
+								);
+							break;
+							
+							case 'color':
+								$wp_customize->add_control(
+									new WP_Customize_Color_Control(
+										$wp_customize,
+										$field['id'],
+										array(
+											'label'      => __( $field['label'], 'branch' ),
+											'section'    => $section['id']
+										)
+									)
+								);
+							break;
+							
+							case 'image':
+								$wp_customize->add_control(
+									new WP_Customize_Image_Control(
+										$wp_customize,
+										$field['id'],
+										array(
+											'label'      => __( $field['label'], 'branch' ),
+											'section'    => $section['id'],
+										)
+									)
+								);
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
    
-   /**
-	* This outputs the javascript needed to automate the live settings preview.
-	* Also keep in mind that this function isn't necessary unless your settings 
-	* are using 'transport'=>'postMessage' instead of the default 'transport'
-	* => 'refresh'
-	* 
-	* Used by hook: 'customize_preview_init'
-	* 
-	* @see add_action('customize_preview_init',$func)
-	* @since branch 1.0
-	*/
-   public static function live_preview() {
-	  wp_enqueue_script( 
-		   'branch-themecustomizer', // Give the script a unique ID
-		   get_template_directory_uri() . '/assets/js/theme-customizer.js', // Define the path to the JS file
-		   array(  'jquery', 'customize-preview' ), // Define dependencies
-		   '', // Define a version (optional) 
-		   true // Specify whether to put in footer (leave this true)
-	  );
-   }
-
 	/**
-	 * This will generate a line of CSS for use in header output. If the setting
-	 * ($mod_name) has no defined value, the CSS will not be output.
+	 * This outputs the javascript needed to automate the live settings preview.
+	 * Also keep in mind that this function isn't necessary unless your settings 
+	 * are using 'transport'=>'postMessage' instead of the default 'transport'
+	 * => 'refresh'
 	 * 
-	 * @uses get_theme_mod()
-	 * @param string $selector CSS selector
-	 * @param string $style The name of the CSS *property* to modify
-	 * @param string $mod_name The name of the 'theme_mod' option to fetch
-	 * @param string $prefix Optional. Anything that needs to be output before the CSS property
-	 * @param string $postfix Optional. Anything that needs to be output after the CSS property
-	 * @param bool $echo Optional. Whether to print directly to the page (default: true).
-	 * @return string Returns a single line of CSS with selectors and a property.
+	 * Used by hook: 'customize_preview_init'
+	 * 
+	 * @see add_action('customize_preview_init',$func)
 	 * @since branch 1.0
 	 */
-	public static function generate_css( $selector, $style, $mod_name, $prefix='', $postfix='', $echo=true ) {
-	  $return = '';
-	  $mod = get_theme_mod($mod_name);
-	  if ( ! empty( $mod ) ) {
-		 $return = sprintf('%s { %s:%s; }',
-			$selector,
-			$style,
-			$prefix.$mod.$postfix
-		 );
-		 if ( $echo ) {
-			echo $return;
-		 }
-	  }
-	  return $return;
+	public function live_preview() {
+		wp_enqueue_script( 
+			'branch-themecustomizer', // Give the script a unique ID
+			get_template_directory_uri() . '/assets/js/theme-customizer.js', // Define the path to the JS file
+			array(
+				'jquery',
+				'customize-preview'
+			), // Define dependencies
+			'', // Define a version (optional) 
+			true // Specify whether to put in footer (leave this true)
+		);
 	}
 }
-
-// bind to BranchSite
-add_action('branch_construct', array('BranchCustomize', 'branch'), 10);
