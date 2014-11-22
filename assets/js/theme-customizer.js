ce9afa5f78eb0a2c13df24026af7035477d5769a
James West <james@jameswest.co.nz>
2014-11-22 21:25:23 +1300

    Added CSS class

--- assets/js/theme-customizer.js
+++ assets/js/theme-customizer.js
@@ -1,19 +1,8 @@
 ( function( $ ) {
-	//on load, refresh all elements
-	/*parent.wp.customize.each(function(obj, key){
-	});*/
-				
 	// Update the site title in real time...
 	wp.customize( 'blogname', function( value ) {
 		value.bind( function( newval ) {
 			$('.site-title').text( newval );
-		} );
-	} );
-	
-	wp.customize( 'skin', function( value ) {
-		value.bind( function( newval ) {
-			// reload the iframe
-			parent.wp.customize.previewer.refresh();
 		} );
 	} );
 } )( jQuery );
---
+++ lib/css.php
@@ -0,0 +1,101 @@
+<?php
+class BranchCSS {
+	private $variables = array();
+	
+	public function __construct($skin) {
+		// make skin available to future calls
+		$this->skin = $skin;
+        
+        add_action('init', array( $this, 'enqueue' ) );
+		
+        // TODO: setup garbage collector
+        //$this->compiler()->install();
+        //$this->compiler()->uninstall();
+	}
+	
+	public function compiler() {
+		/*add_filter('wp_less_compiler', function(){
+			return 'less.php';
+		});*/
+		
+		if(!isset($this->compiler)) {
+			$this->compiler = WPLessPlugin::getInstance();
+		}
+		
+		return $this->compiler;
+	}
+	
+	public function enqueue() {
+		global $wp_styles;
+			
+		// only enqueue theme styles to front-end site
+		if(!is_admin()) {
+			$this->add_customizer_variables();
+			
+		    $lessConfig = $this->compiler()->getConfiguration();
+		
+		    // compiles in the active theme, in a ‘compiled-css’ subfolder
+		    $lessConfig->setUploadDir($this->skin->path() . '/tmp');
+		    $lessConfig->setUploadUrl($this->skin->uri() . '/tmp');
+		    
+		    // change compile path if we're in customizer
+		    if(isset($_POST['customized'])) {
+			    $lessConfig->setUploadDir($this->skin->path() . '/tmp/customized');
+			    $lessConfig->setUploadUrl($this->skin->uri() . '/tmp/customized');
+		    }
+		
+			wp_enqueue_style('branch-main', $this->skin->uri() . '/skin.less');
+		
+			// set default global variables
+			$this->add_variables(array(
+				'skin_uri' => '"' . $this->skin->uri() . '"'
+			));
+			
+			$this->compiler()->setVariables($this->variables);
+			$this->compiler()->dispatch();
+		}
+	}
+	
+	private function add_variables($vars) {
+		$this->variables = array_merge($vars, $this->variables);
+		return $this->variables;
+	}
+	
+	private function add_customizer_variables() {
+		if(!isset($this->skin->config()['customize']) || !isset($this->skin->config()['customize']['sections'])) return;
+		
+		$vars = array();
+		
+		foreach($this->skin->config()['customize']['sections'] as $section) {
+			if(isset($section['fields'])) {
+				foreach($section['fields'] as $field) {
+					if(!isset($field['id']) || !isset($field['default'])) continue;
+					
+					$id = preg_replace('/[^a-zA-Z\_]+/', '', str_replace('-', '_', $field['id']));
+					
+					$value = get_theme_mod($field['id'], $field['default']);
+					
+					// get value from $_POST['customized'] if it exists
+					if(isset($_POST['customized'])) {// && 
+						$customized = json_decode( wp_unslash( $_POST['customized'] ), true );
+						
+						if(isset($customized[$field['id']])) {
+							$value = $customized[$field['id']];
+						}
+					}
+					
+					// in case the value is empty...
+					if(empty($value)) {
+						$value = $field['default'];
+					}
+					
+					if(isset($field['css']) && $field['css'] === true && $id != '' && $value != '') {
+						$vars[$id] = $value;
+					}
+				}
+			}
+		}
+		
+		return $this->add_variables($vars);
+	}
+}
