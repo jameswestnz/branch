7651c076678a1fe68681500535e871a791e36969
James West <james@jameswest.co.nz>
2014-11-25 09:53:47 +1300

    Added Jetpack widget visibility

--- lib/jetpack.php
+++ lib/jetpack.php
@@ -13,22 +13,30 @@ class Jetpack extends \Branch\Singleton {
 			require_once locate_template('lib/vendor/Automattic/jetpack/class.jetpack-user-agent.php');
 		}
 		
+		// Custom CSS
 		if(!class_exists('Jetpack_Custom_CSS')) {
 			// include jetpack custom css class
 			require_once locate_template('lib/vendor/Automattic/jetpack/modules/custom-css.php');
 		
 			// not sure what this does? copied from Jetpack core
 			//add_action( 'init', array( 'Jetpack_Custom_CSS', 'disable' ), 11 );
-			
-			// need to modify plugins_url as we're not a plugin
-			add_filter( 'plugins_url', function($url, $path, $plugin){
-				if(strpos($url, 'lib/vendor/Automattic/jetpack') === false) return $url;
-				
-				$url = explode('lib/vendor/Automattic/jetpack', $url);
-				$url = get_template_directory_uri() . '/lib/vendor/Automattic/jetpack' . $url[1];
-				
-				return $url;
-			}, 3, 100);
 		}
+		
+		// Widget Visibility
+		if(!class_exists('Jetpack_Widget_Conditions')) {
+			// include jetpack custom css class
+			require_once locate_template('lib/vendor/Automattic/jetpack/modules/widget-visibility.php');
+		}
+		
+		// Fix asset locations
+		// need to modify plugins_url as we're not a plugin
+		add_filter( 'plugins_url', function($url, $path, $plugin){
+			if(strpos($url, 'lib/vendor/Automattic/jetpack') === false) return $url;
+			
+			$url = explode('lib/vendor/Automattic/jetpack', $url);
+			$url = get_template_directory_uri() . '/lib/vendor/Automattic/jetpack' . $url[1];
+			
+			return $url;
+		}, 3, 100);
 	}
 }
