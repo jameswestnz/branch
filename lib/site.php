9361b7587409dfbcd5c324d7b63bc0c53f1b9a0d
James West <james@jameswest.co.nz>
2014-11-27 10:50:14 +1300

    Moved Jetpack modules into future feature branch for further development

--- lib/site.php
+++ lib/site.php
@@ -5,48 +5,14 @@ require_once 'singleton.php';
 
 class Site extends \Branch\Singleton {
 	/**
-	 * includes
-	 *
-	 * The $includes array determines the code library included in your theme.
-	 * Add or remove files to the array as needed. Supports child theme overrides.
-	 *
-	 * Please note that missing files will produce a fatal error.
-	 */
-	private $includes = array(
-		'lib/vendor/jarednova/timber-library/timber.php',
-		'lib/twig.php',
-		'lib/skin.php',
-		'lib/css.php',
-		'lib/breadcrumbs.php',
-		'lib/customize.php',
-		'lib/shortcodes.php',
-		'lib/jetpack.php',
-		'lib/vendor/oncletom/wp-less/bootstrap-for-theme.php'
-	);
-	
-	/**
 	 * __construct function.
 	 * 
 	 * @access public
 	 * @return void
 	 */
 	public function __construct(){
-		//include libs
-		$this->include_libs();
-		
-		// load skin
-		$this->skin();
-		
-		// load jetpack
-		$this->jetpack();
-		
-		// setup timber/twig
-		$this->twig();
-	
-		// add theme support etc
-		add_theme_support('post-formats');
-		add_theme_support('post-thumbnails');
-		add_theme_support('menus');
+		add_action('branch_construct', array($this, 'include_libs'), 99);
+		add_action('branch_construct', array($this, 'init'), 100);
 		
 		// register default menus
 		add_action('init', function(){
@@ -68,6 +34,21 @@ class Site extends \Branch\Singleton {
 		// disable Timber updates
 		add_filter('site_transient_update_plugins', array($this, 'disable_timber_updates'));
 		
+		do_action('branch_construct');
+	}
+	
+	public function init() {
+		// load skin
+		$this->skin();
+		
+		// setup timber/twig
+		$this->twig();
+	
+		// add theme support etc
+		add_theme_support('post-formats');
+		add_theme_support('post-thumbnails');
+		add_theme_support('menus');
+		
 		// enable timber caching
 		if (!WP_DEBUG){
 			$cache = apply_filters('branch_cache', true);
@@ -77,7 +58,7 @@ class Site extends \Branch\Singleton {
 	
 	// can't have the timber plugin updated without us being aware of it
 	// this will need to be a bit more "dynamic" in the future, in the fact that it should be dependant on version numbers i.e branch version 2.2 is compatible up to timber 3
-	function disable_timber_updates($plugins) {
+	public function disable_timber_updates($plugins) {
 		if(isset($plugins->response)) {
 			try {
 		    	unset($plugins->response['timber-library/timber.php']);
@@ -87,8 +68,19 @@ class Site extends \Branch\Singleton {
 	}
 	
 	// might make this an autoloader form the lib folder - no point in manually adding to an array, although this method may be more secure?
-	function include_libs() {
-		foreach ($this->includes as $file) {
+	public function include_libs() {
+		$includes = apply_filters('branch_includes', array(
+			'lib/vendor/jarednova/timber-library/timber.php',
+			'lib/twig.php',
+			'lib/skin.php',
+			'lib/css.php',
+			'lib/breadcrumbs.php',
+			'lib/customize.php',
+			'lib/shortcodes.php',
+			'lib/vendor/oncletom/wp-less/bootstrap-for-theme.php'
+		));
+		
+		foreach ($includes as $file) {
 			if (!$filepath = locate_template($file)) {
 				trigger_error(sprintf(__('Error locating %s for inclusion'), $file), E_USER_ERROR);
 			}
@@ -98,11 +90,11 @@ class Site extends \Branch\Singleton {
 		unset($file, $filepath);
 	}
 
-	function register_post_types(){
+	public function register_post_types(){
 		//this is where you can register custom post types
 	}
 
-	function register_taxonomies(){
+	public function register_taxonomies(){
 		//this is where you can register custom taxonomies
 	}
 	
@@ -113,7 +105,7 @@ class Site extends \Branch\Singleton {
 	 * @param mixed $context
 	 * @return void
 	 */
-	function add_to_context($context){
+	public function add_to_context($context){
 		global $user_identity;
 		$context['site'] = $this;
 		$context['user_identity'] = $user_identity;
@@ -151,20 +143,6 @@ class Site extends \Branch\Singleton {
 		}
 		
 		return $this->skin;
-	}
-	
-	/**
-	 * jetpack function.
-	 * 
-	 * @access private
-	 * @return void
-	 */
-	private function jetpack() {
-		if(!isset($this->jetpack)) {
-			$this->jetpack = \Branch\Jetpack::instance($this);
-		}
-		
-		return $this->jetpack;
 	}
 	
 	/**
